3fb6d78a1ee2da0ab5bffc99761d39a5c3fe490a
Piyush762051 <khandarepiyush2@gmail.com>
2024-12-03 15:08:50 +0530

    create Project

---
+++ ProjectGitHub/src/main/java/com/cjc/ProjectGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGitHubApplication.class, args);
+	}
+
+}
